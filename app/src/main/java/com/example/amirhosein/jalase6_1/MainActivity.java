9d42d5c3162001ad48261d2908ed5aca4605b90a
A.S <amirhosensoleymani@gmail.com>
2016-11-16 01:16:53 +0330

    Initial commit

---
+++ app/src/main/java/com/example/amirhosein/jalase6_1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.amirhosein.jalase6_1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button create = (Button) findViewById(R.id.create);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Enlist.class);
+                startActivity(intent);
+                
+            }
+        });
+        Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginFirst.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
